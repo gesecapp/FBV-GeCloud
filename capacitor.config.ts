20a366214cb4280c937d347107fd94b283196a74
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2026-04-09 13:49:23 -0300

    feat: v1.40

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  // appId: 'br.com.gecloud',
-  appId: 'com.gesec',
+  appId: 'br.com.gecloud',
+  // appId: 'com.gesec',
   appName: 'GeCloud',
   webDir: 'dist',
   zoomEnabled: false,
